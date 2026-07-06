7b66f334dce1df65d3b36e6bc7ea0a58722c70a5
seungyunpark <115516297+sypark1995@users.noreply.github.com>
2026-07-06 16:35:29 +0900

    feat: GetStockProfileUseCase, GetStockMetricsUseCase, GetPeersUseCase 추가

---
+++ core/domain/src/main/kotlin/com/sypark/finnhub/core/domain/usecase/detail/GetStockProfileUseCase.kt
@@ -0,0 +1,12 @@
+package com.sypark.finnhub.core.domain.usecase.detail
+
+import com.sypark.finnhub.core.common.AppResult
+import com.sypark.finnhub.core.domain.model.StockProfile
+import com.sypark.finnhub.core.domain.repository.MarketRepository
+import javax.inject.Inject
+
+class GetStockProfileUseCase @Inject constructor(
+    private val repository: MarketRepository,
+) {
+    suspend operator fun invoke(symbol: String): AppResult<StockProfile> = repository.getStockProfile(symbol)
+}
